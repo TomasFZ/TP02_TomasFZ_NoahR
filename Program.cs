5115fa2592ad20f77b835c8007b9d964209db3eb
TOMAS FERNANDEZ ZABALA <47394082@ort>
2023-04-10 08:07:50 -0300

    version 2

--- Program.cs
+++ Program.cs
@@ -4,34 +4,38 @@ class Program
     static void Main(string[] args)
     {
         int dni = 0;
+        int dniBuscar = 0;
         string apellido = "";
         string nombre = "";
         int numMenu = 0;
         DateTime fechaNacimiento = new DateTime();
         string email = "";
         List<Persona> listaPersonas = new List<Persona>();
+        int iterador = 0;
         do{
         numMenu = IngresarInt("Ingrese un numero para seleccionar una opción. 1. Cargar nueva persona. 2. Obtener características del censo. 3. Buscar persona. 4. Modificar mail de una persona. 5. Salir");
         switch(numMenu){
             case 1: 
-            
-            dni = IngresarInt("Ingrese el dni de la persona: ");
+            iterador++;
+            dni = IngresarInt("Ingrese el dni de la persona: "); //verificar que no haya doble dni y que los datos sean correctos. 
             apellido = IngresarString("Ingrese el apellido: ");
             nombre = IngresarString("Ingrese el nombre: ");
             fechaNacimiento = IngresarDateTime("Ingrese su fecha de nacimiento de la siguiente forma: x/xx/xx");
             email = IngresarString("Ingrese su Email: ");
             Persona p = new Persona(dni,apellido,nombre,fechaNacimiento, email);
             Console.WriteLine(fechaNacimiento);
-            p.ObtenerEdad(); //se usan en el punto 2. 
             listaPersonas.Add(p);
             Console.WriteLine($"Se ha creado a la persona {p.Apellido} {p.Nombre} y se ha agregado a la lista.");
 
-            foreach(Persona y in listaPersonas){ //despues se saca. 
-                Console.WriteLine(y.Nombre);
-            }
+            // foreach(Persona y in listaPersonas){ //despues se saca. 
+            //     Console.WriteLine(y.Nombre);
+            // }
             break;
 
             case 2: 
+            if(iterador == 0){
+                Console.WriteLine("Aún no se ingresaron personas en la lista.");
+            }else{
             int numVotar=0;
             double promedioEdad = 0;
             int num1 = 0;
@@ -52,15 +56,39 @@ class Program
             }
             promedioEdad = num1/i;
             Console.WriteLine($"El promedio de la edad es: {promedioEdad}");
+            }
             break;
 
             case 3: 
-            
+            int edad = 0;
+            dniBuscar = IngresarInt("Ingrese el DNI de la persona a buscar: ");
+            foreach(Persona t in listaPersonas){
+                if(dniBuscar == t.DNI){
+                    edad = t.ObtenerEdad();
+                    Console.WriteLine("NOMBRE: " + t.Nombre);
+                    Console.WriteLine("APELLIDO: " + t.Apellido);
+                    Console.WriteLine("EDAD: " + edad);
+                    Console.WriteLine("EMAIL: " + t.Email);
+                    //Console.WriteLine("PUEDE VOTAR: " + t.PuedeVotar());
+                }else{
+                    Console.WriteLine("No se encuentra el DNI. ");
+                }
+            }
             break;
 
             case 4: 
+            string newMail = "";
+            dniBuscar = IngresarInt("Ingrese el DNI de la persona a buscar: ");
+            foreach(Persona r in listaPersonas){
+                if(dniBuscar == r.DNI){
+                    newMail = IngresarString("Ingrese el nuevo mail del usuario: ");
+                    r.Email = newMail;
+                }else{
+                    Console.WriteLine("No se encuentra el DNI. ");
+                }
+            }
             break;
-
+            
             case 5: 
             break;
         }
